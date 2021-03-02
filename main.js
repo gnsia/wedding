19e7186b5d2d6068f92100278304f4c2599484de
gnsia <gnsia@naver.com>
2021-03-02 14:47:28 +0900

    modal update

--- main.js
+++ main.js
@@ -1,8 +1,4 @@
 let modal = document.querySelector('#modal');
-let thumbnail = document.querySelector('.thumnail');
-let imgUrl = document.querySelectorAll('.thumbnail-image');
-let modalContent = document.querySelector('.modal-content');
-let modalUrl = document.querySelector('.pop').src;
 
 function modalOpen(self) {
   document.querySelector('.pop').src = self.src;
