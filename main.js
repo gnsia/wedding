e5ec3c9082786f3208f95830b619cebce39c536d
gnsia <gnsia@naver.com>
2021-03-02 14:54:19 +0900

    Revert "modal update"
    This reverts commit 19e7186b5d2d6068f92100278304f4c2599484de.

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
 let modal = document.querySelector('#modal');
+let thumbnail = document.querySelector('.thumnail');
+let imgUrl = document.querySelectorAll('.thumbnail-image');
+let modalContent = document.querySelector('.modal-content');
+let modalUrl = document.querySelector('.pop').src;
 
 function modalOpen(self) {
   document.querySelector('.pop').src = self.src;
