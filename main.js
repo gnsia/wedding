69aaa20dae735ebc7e2f3ffc892afa728f0ee49c
gnsia <gnsia@naver.com>
2021-03-24 18:18:00 +0900

    test
    test

--- main.js
+++ main.js
@@ -15,34 +15,9 @@ function modalClose() {
 
 
 
+//scroll opacity
 
-//scroll
 
-addEventListener('scroll', function(e){
-  let scrollYPos = Math.round(scrollY/30)/10;
-  console.log(scrollY);
-  opacityUpNames(scrollYPos);
-  opacityUpMessage(scrollY);
-})
 
-function opacityUpNames(num) {
-  document.querySelector('.boy').style.opacity = num;
-  document.querySelector('.girl').style.opacity = num;
-}
+//scroll zoom
 
-function opacityUpMessage(num) {
-  if (num > 1000) {
-    document.querySelector('.message').style.position = 'sticky';
-    document.querySelector('.message').style.top = '5%';
-  } else {
-    document.querySelector('.message').style.position = 'relative';
-    document.querySelector('.message').style.top = '1000px';
-  }
-  if (num > 777) {
-    document.querySelector('i').style.color = 'rgba(255, 0, 0,0.4)';
-    document.querySelector('.heart').style.opacity = 0;
-  } else {
-    document.querySelector('i').style.color = 'lightsteelblue';
-    document.querySelector('.heart').style.opacity = 1;
-  }
-}
