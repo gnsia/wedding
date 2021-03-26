28d9fc29c5d7819248f36f65523ed027220ad1e1
gnsia <gnsia@naver.com>
2021-03-26 11:03:29 +0900

    add kakaomap test

--- main.js
+++ main.js
@@ -35,3 +35,39 @@ function modalClose() {
 }
 
 
+////////////////////////////////////// kakao map
+// 클릭한 위치의 위도는 37.54490486081645 이고, 경도는 127.00042434032412 입니다
+
+const x = 37.54490486081645;
+const y = 127.00042434032412;
+
+var mapContainer = document.getElementById('map-weddinghall'), // 지도를 표시할 div 
+    mapOption = { 
+        center: new kakao.maps.LatLng(x, y), // 지도의 중심좌표
+        level: 3 // 지도의 확대 레벨
+    };
+
+var map = new kakao.maps.Map(mapContainer, mapOption);
+
+// 마커가 표시될 위치입니다 
+var markerPosition  = new kakao.maps.LatLng(x, y); 
+
+// 마커를 생성합니다
+var marker = new kakao.maps.Marker({
+    position: markerPosition
+});
+
+// 마커가 지도 위에 표시되도록 설정합니다
+marker.setMap(map);
+
+var iwContent = '<div style="padding:5px;">여기에요! <br><a href="https://map.kakao.com/link/map/Hello World!,33.450701,126.570667" style="color:blue" target="_blank">큰지도보기</a> <a href="https://map.kakao.com/link/to/Hello World!,33.450701,126.570667" style="color:blue" target="_blank">길찾기</a></div>', // 인포윈도우에 표출될 내용으로 HTML 문자열이나 document element가 가능합니다
+    iwPosition = new kakao.maps.LatLng(x, y); //인포윈도우 표시 위치입니다
+
+// 인포윈도우를 생성합니다
+var infowindow = new kakao.maps.InfoWindow({
+    position : iwPosition, 
+    content : iwContent 
+});
+  
+// 마커 위에 인포윈도우를 표시합니다. 두번째 파라미터인 marker를 넣어주지 않으면 지도 위에 표시됩니다
+infowindow.open(map, marker); 
