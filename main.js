21cc7a0c5b272e2a4945cd60f18982b8985b6fcb
gnsia <gnsia@naver.com>
2021-04-04 02:35:02 +0900

    delete copy clipboard account

--- main.js
+++ main.js
@@ -17,17 +17,17 @@ document.querySelector('.clipboard1').onclick = function(self) {
   alert('복사완료');
 }
 
-document.querySelector('.clipboard2').onclick = function(self) {
-  let copyVal = self.target.value;
-  clipboardCopyTo(copyVal);
-  alert('복사완료');
-}
+// document.querySelector('.clipboard2').onclick = function(self) {
+//   let copyVal = self.target.value;
+//   clipboardCopyTo(copyVal);
+//   alert('복사완료');
+// }
 
-document.querySelector('.clipboard3').onclick = function(self) {
-  let copyVal = self.target.value;
-  clipboardCopyTo(copyVal);
-  alert('복사완료');
-}
+// document.querySelector('.clipboard3').onclick = function(self) {
+//   let copyVal = self.target.value;
+//   clipboardCopyTo(copyVal);
+//   alert('복사완료');
+// }
 
 
 ////////////////////////////////////////////////////////////////////// scroll
@@ -142,56 +142,3 @@ class myStaticMap{
 
 const weddinghall = new myStaticMap(37.54490486081645, 127.00042434032412, 'map-weddinghall');
 
-
-/////////////////////////////////////////////////img thumbnail
-window.addEventListener("load", function(e) {
-  const imgUrl = [
-    'https://i.ibb.co/1RZyvQj/210317-0089.jpg',
-    'https://i.ibb.co/FYfRQxf/210317-0019.jpg',
-    'https://i.ibb.co/6gMPfYB/210317-0173.jpg',
-    'https://i.ibb.co/0cWjL0v/210317-0154.jpg',
-    'https://i.ibb.co/pPY7J9t/210317-0194.jpg',
-    'https://i.ibb.co/g9g7dw7/210317-0217.jpg',
-    'https://i.ibb.co/HYKgjWw/210317-0341.jpg',
-    'https://i.ibb.co/Zh8nkb0/210317-0409.jpg',
-    'https://i.ibb.co/XFK90wm/210317-0552.jpg',
-    'https://i.ibb.co/6DfF1WD/210317-0618.jpg',
-    'https://i.ibb.co/vkN2xpy/210317-0763.jpg',
-    'https://i.ibb.co/VQmgC7v/210317-0801.jpg',
-    'https://i.ibb.co/d0r2Jfj/210317-0818.jpg',
-    'https://i.ibb.co/jRDV2hq/210317-0859.jpg',
-    'https://i.ibb.co/QPKfktG/210317-1102.jpg',
-    'https://i.ibb.co/1ZZNzD2/210317-1143.jpg',
-    'https://i.ibb.co/5kCr4kp/210317-1151.jpg',
-    'https://i.ibb.co/FKGk7PM/210317-1237.jpg',
-    'https://i.ibb.co/3rgFC6f/210317-1288.jpg',
-    'https://i.ibb.co/YjKcB1D/210317-1344.jpg',
-    'https://i.ibb.co/xSBY54n/210317-1449.jpg',
-    'https://i.ibb.co/yNJGd1J/210317-1508.jpg',
-    'https://i.ibb.co/j5fpRjn/210317-1531.jpg',
-    'https://i.ibb.co/c6FBCpK/210317-1545.jpg',
-    'https://i.ibb.co/Nxc3LvB/210317-1625.jpg',
-    'https://i.ibb.co/RH0mSY7/210317-1630.jpg',
-    'https://i.ibb.co/jrqCj3G/210317-1642.jpg',
-    'https://i.ibb.co/CWGHxZ8/210317-1682.jpg',
-    'https://i.ibb.co/1T5K9bv/210317-1776.jpg',
-    'https://i.ibb.co/1fBMdT5/210317-1783.jpg'
-  ]
-  console.log(imgUrl.length);
-
-
-  document.querySelector('.left').innerText = '<h1>hello</h1>';
-  document.getElementById('left').appendChild('h1').innerText = "hello";
-
-  thumbnail_left.innerHTML = `<button type="button" class="btn" data-bs-toggle="modal" data-bs-target="#left-1">
-  <img src="https://i.ibb.co/FYfRQxf/210317-0019.jpg" class="img-thumbnail" alt="...">
-  </button>`
-  
-
-  
-  // for (let i = 0; i < imgUrl; i++) {
-  //   thumbnail_left.innerHTML = `<button type="button" class="btn" data-bs-toggle="modal" data-bs-target="#left-1">
-  //   <img src="${imgUrl[i]}" class="img-thumbnail" alt="...">
-  //   </button>`
-  // }
-}, false);
